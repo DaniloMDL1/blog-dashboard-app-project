409c872b2e1543e1e1faa1fd3c41d94b37328bdc
DaniloMDL1 <danilodfl016@gmail.com>
2024-03-09 14:20:15 +0100

    update user profile route

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,3 +1,7 @@
+import User from "../models/userModel.js"
+import bcrypt from "bcryptjs"
+import { v2 as cloudinary } from "cloudinary"
+
 export const updateUserProfile = async (req, res) => {
     try {
         const { fullName, username, email, password } = req.body
@@ -5,6 +9,29 @@ export const updateUserProfile = async (req, res) => {
         const { userId } = req.params
         const loggedInUserId = req.user.userId
 
+        let user = await User.findById(userId)
+        if(!user) return res.status(404).json({ error: "User not found." })
+
+        if(userId !== loggedInUserId.toString()) return res.status(403).json({ error: "User is not allowed to update other user's profile."})
+
+        if(password) {
+            const salt = await bcrypt.genSalt(10)
+            const passwordHash = await bcrypt.hash(password, salt)
+            user.password = passwordHash
+        }
+
+        if(profilePicture) {
+            const uploadedResponse = await cloudinary.uploader.upload(profilePicture)
+            profilePicture = uploadedResponse.secure_url
+        }
+
+        user.fullName = fullName || user.fullName
+        user.username = username || user.username
+        user.email = email || user.email
+        user.profilePicture = profilePicture || user.profilePicture
+
+        user = await user.save()
+        res.status(200).json(user)
         
     } catch(error) {
         console.log(error)
--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,13 @@ app.use(express.json({ limit: "30mb" }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cookieParser())
 
+// CLOUDINARY
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
 // ROUTES
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
